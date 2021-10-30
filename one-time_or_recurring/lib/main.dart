f5d4a6d678cab42332218a4712b25a0bff33a50d
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-10-30 10:07:31 +0200

    Add files via upload

---
+++ one-time_or_recurring/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Example());
+}
+
+class Example extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+//  List<String> _locations = ['Please choose a location', 'A', 'B', 'C', 'D']; // Option 1
+//  String _selectedLocation = 'Please choose a location'; // Option 1
+  List<String> _locations = [
+    'One-Time',
+    'Recurring'
+  ]; // Option 2
+  String _selectedLocation; // Option 2
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: DropdownButton(
+            hint: Text('Is this a one-time or recurring expense?'), // Not necessary for Option 1
+            value: _selectedLocation,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedLocation = newValue;
+              });
+            },
+            items: _locations.map((location) {
+              return DropdownMenuItem(
+                child: new Text(location),
+                value: location,
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
