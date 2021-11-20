397ced15e011b4cb300b1f232d940a90f7a9964a
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-11-20 23:09:36 +0200

    updated constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+//const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+var mainColorList = [
+  const Color(0xFF92CCFE),
+  const Color(0xFFECF4FB),
+  const Color(0xFF0938B6),
+  const Color(0xFF67B5FD),
+  const Color(0xFFFFFFFA),
+  const Color(0xFFA6A6B2),
+];
+
+var iconsColor = const Color(0xFF5689B9);
+var progressBarColorDark = const Color(0xFFF6BAB5);
+var progressbarColorWhite = const Color(0xFFED7470);
+
+//padding
+var  buttonPadding = const EdgeInsets.symmetric(vertical: 15, horizontal: 100);
+
+Map<int, Color> color = {
+  50: const Color.fromRGBO(18, 60, 175, .1),
+  100: const Color.fromRGBO(18, 60, 175, .2),
+  200: const Color.fromRGBO(18, 60, 175, .3),
+  300: const Color.fromRGBO(18, 60, 175, .4),
+  400: const Color.fromRGBO(18, 60, 175, .5),
+  500: const Color.fromRGBO(18, 60, 175, .6),
+  600: const Color.fromRGBO(18, 60, 175, .7),
+  700: const Color.fromRGBO(18, 60, 175, .8),
+  800: const Color.fromRGBO(18, 60, 175, .9),
+  900: const Color.fromRGBO(18, 60, 175, 1),
+};
+
+/*900: const Color.fromRGBO(86, 137, 185, 1), 900: const Color.fromRGBO(9, 57, 182, 1),*/
