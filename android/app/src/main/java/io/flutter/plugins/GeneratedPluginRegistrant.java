7415f761323f9e3fe0e518e66840c791749a23d8
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-12-02 12:31:04 +0200

    added keyboard handling, income/expense selection, logout button, recurring, validations for all form fields, date formatting changed, and saving income to db, fixed bug with saving selected category id to db

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -30,5 +30,10 @@ public final class GeneratedPluginRegistrant {
     } catch(Exception e) {
       Log.e(TAG, "Error registering plugin firebase_core, io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin", e);
     }
+    try {
+      flutterEngine.getPlugins().add(new com.jrai.flutter_keyboard_visibility.FlutterKeyboardVisibilityPlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin flutter_keyboard_visibility, com.jrai.flutter_keyboard_visibility.FlutterKeyboardVisibilityPlugin", e);
+    }
   }
 }
