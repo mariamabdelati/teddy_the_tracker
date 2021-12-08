50d20fee7359eccda120bd5dbb579d1e3f7669a9
George Francis <georgeemadfrancis@gmail.com>
2021-12-08 02:28:57 +0200

    started working on the line graphs, will revert the entries class back to how it was

--- lib/screens/registration/auth_screen.dart
+++ lib/screens/registration/auth_screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: deprecated_member_use, avoid_print
 
-<<<<<<<< HEAD:lib/screens/registration/auth_screen.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,18 +8,6 @@ import '../../screens/welcome/welcome_page.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen(this._isLogin);
-========
-import 'package:teddy_the_tracker/screens/dashboard_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:teddy_the_tracker/constants.dart';
-import 'package:teddy_the_tracker/widgets/auth/auth_form.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-class AuthScreen extends StatefulWidget {
-  AuthScreen(this._isLogin);
->>>>>>>> 161991911ed4ab27b25a9a037201abb4b04b33d9:lib/screens/auth_screen.dart
   final bool _isLogin;
 
   @override
@@ -32,12 +19,12 @@ class _AuthScreenState extends State<AuthScreen> {
   bool _isLoading = false;
 
   void _submitAuthForm(
-      String email,
-      String username,
-      String password,
-      bool isLogin,
-      BuildContext ctx,
-      ) async {
+    String email,
+    String username,
+    String password,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
     //auth logic is here;
     UserCredential authResult;
     try {
@@ -57,16 +44,10 @@ class _AuthScreenState extends State<AuthScreen> {
           "username": username,
           "email": email,
         });
-
-        // create wallet(uid) DON'T FORGET IT OR MARIAM WILL KILL YOU
       }
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => const DashboardScreen()),
-<<<<<<<< HEAD:lib/screens/registration/auth_screen.dart
-              (route) => false);
-========
           (route) => false);
->>>>>>>> 161991911ed4ab27b25a9a037201abb4b04b33d9:lib/screens/auth_screen.dart
     } on FirebaseAuthException catch (err) {
       var msg = "Error occurred, please check your credentials";
 
@@ -94,13 +75,8 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<<< HEAD:lib/screens/registration/auth_screen.dart
       backgroundColor: Theme.of(context).primaryColor,
       body: LoginPage(
-========
-      backgroundColor: mainColorList[0],
-      body: AuthForm(
->>>>>>>> 161991911ed4ab27b25a9a037201abb4b04b33d9:lib/screens/auth_screen.dart
         _submitAuthForm,
         _isLoading,
         widget._isLogin,
@@ -108,5 +84,6 @@ class _AuthScreenState extends State<AuthScreen> {
     );
   }
 }
+
 
 // storing Extra user data 5:35;
