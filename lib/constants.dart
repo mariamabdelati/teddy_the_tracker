df784ac7c2bd6bf7a6823bb94d6357d77449885c
George Francis <72275222+George-Francis11@users.noreply.github.com>
2022-01-05 00:34:48 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
-//const kPrimaryColor = Color(0xFF6F35A5);
-//const kPrimaryLightColor = Color(0xFFF1E6FF);
+
 
 var mainColorList = [
   const Color(0xFF92CCFE),
-  //const Color(0xFFD1EAFF),
   const Color(0xFFECF4FB),
-  //const Color(0xFF0054DA),
   const Color(0xFF0C41CD),
-  //const Color(0xFF0938B6),
   const Color(0xFF67B5FD),
   const Color(0xFFFFFFFA),
   const Color(0xFFA6A6B2),
@@ -19,16 +15,6 @@ var mainColorList = [
 ];
 
 var pieChartColors =[
-  /*const Color(0xFF3493E3),
-  const Color(0xFF97C7EF),
-  const Color(0xFF61ADEB),
-  const Color(0xFF388EE3),
-  const Color(0xFF3498F7),
-  const Color(0xFF1491FF),
-  const Color(0xFF92C9EC),
-  const Color(0xFFD1EAFF),
-  const Color(0xFFE8F5FE),
-  const Color(0xFF1D67A6),*/
   const Color(0xFF9DFBB1),
   const Color(0xFFFFC8C2),
   const Color(0xFFFFF1C2),
@@ -60,5 +46,3 @@ Map<int, Color> color = {
   800: const Color.fromRGBO(18, 60, 175, .9),
   900: const Color.fromRGBO(18, 60, 175, 1),
 };
-
-/*900: const Color.fromRGBO(86, 137, 185, 1), 900: const Color.fromRGBO(9, 57, 182, 1),*/
